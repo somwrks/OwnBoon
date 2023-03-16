4a85ea2f2aacc67a5834e4ccb9d43639d82a0351
Som <aishwarysrivastava2006@gmail.com>
2023-03-16 17:05:40 +0530

    Preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  <div>
+    <Head>
+      <meta
+        name="description"
+        content="OwnBoon is a self-improvement and productivity platform that empowers individuals to reach their full potential. Our mission is to make the impossible possible, by providing users with the tools and resources they need to unlock their fears, rise above challenges, and achieve their goals. "
+      />
+      <meta charSet="UTF-8" />
+      <meta
+        name="keywords"
+        content="ownboon, ownboon self improvement, ownboon self development, ownboon productivity, ownboon linkedin, ownboon github"
+      />
+      <meta name="author" content="OwnBoon" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Component {...pageProps} />
+  </div>;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,46 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>OwnBoon</title>
+        <link rel="icon" href="/logo.png" />
+        
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <main className=" justify-center text-center flex flex-col">
+        <h1 className="text-9xl text-white font-title">
+          Own<span className="text-[#00b6b6]">Boon</span>{" "}
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2 className="text-white text-2xl font-tag">Rise High With Ownboon</h2>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <div className="container  flex flex-wrap flex-col  text-center ">
+        <span className="text-[40px] font-semibold  animate-character">
+          Coming Soon.
+        </span>
+      </div>
+
+      <main className="container p-3 flex flex-row justify-center">
+        <FontAwesomeIcon className="social-icons p-2" icon={faLinkedin} />
+        <FontAwesomeIcon className="social-icons" icon={faGithub} />
+      </main>
+      <main className="container text-center p-5 flex flex-col justify-center">
+        <p className="text-white font-semibold text-lg leading-loose">
+          OwnBoon is a self-improvement and productivity platform <br /> that empowers
+          individuals to reach their full potential. <br />
+          Stay Tuned.
+        </p>
+      </main>
+      <footer className="flex fixed bottom-0 h-24 w-full items-center text-white justify-center border-t">
+        ©️OwnBoon 2023
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
