24dd9814657650855409479c950238ca2395c187
Som <aishwarysrivastava2006@gmail.com>
2023-03-22 23:37:10 +0530

    Hello

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Navbar from "../components/navbar"
 
 function MyApp({ Component, pageProps }: AppProps) {
 
