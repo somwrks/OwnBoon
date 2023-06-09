9bee8b288f77c53464b54387e80e31086d4354a1
Som <aishwarysrivastava2006@gmail.com>
2023-06-10 01:35:12 +0530

    Hello

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,27 +5,20 @@ export default async function handler(req, res) {
 
     const { email } = req.body;
 
-   
-    // const transporter = await nodemailer.createTransport({
-    //   host: "smtp.zoho.in",
-    //   port: 465,
-    //   secure: true,
-    //   auth: {
-    //     user: "mail@ownboon.com",
-    //     pass: password,
-    //   },
-    // });
     const transporter = await nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.zoho.in",
+      port: 465,
+      secure: true,
       auth: {
-        user: "somdeveloper.queries@gmail.com",
-        pass: "lmcasognjjbxesjw",
+        user: "mail@ownboon.com",
+        pass: password,
       },
+      debug: true,
     });
 
     try {
       const mailOptions = {
-        from: "somdeveloper.queries@gmail.com", 
+        from: "mail@ownboon.com", 
         to: email,
         subject: "Beta Access Sign Up @Ownboon",
         html: "<h1 style='font-weight: bold; font-size: 24px;'>Thank you for signing up for beta access!</h1> <h2 style='font-weight: normal; font-size: 19px;'>Stay tuned around Late June! 🫡</h2> <br/> <br/> <h2 style='font-weight: semi-bold; font-size: 19px;'> Join our discord server: <a href='https://discord.gg/nxeUX3Uufn'>https://discord.gg/nxeUX3Uufn</a></h2> <br/><h2 style='font-weight: semi-bold; font-size: 19px;'> Business Queries Only on- <a href='mailto:business@ownboon.com'>business@ownboon.com</a></h2>",
