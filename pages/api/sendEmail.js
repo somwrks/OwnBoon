a6927eb16487a96d5c62866d77ad91ca8c9948bd
Som <aishwarysrivastava2006@gmail.com>
2023-06-10 01:30:02 +0530

    Hello

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 
     const { email } = req.body;
 
+   
     // const transporter = await nodemailer.createTransport({
     //   host: "smtp.zoho.in",
     //   port: 465,
@@ -24,7 +25,7 @@ export default async function handler(req, res) {
 
     try {
       const mailOptions = {
-        from: "mail@ownboon.com", 
+        from: "somdeveloper.queries@gmail.com", 
         to: email,
         subject: "Beta Access Sign Up @Ownboon",
         html: "<h1 style='font-weight: bold; font-size: 24px;'>Thank you for signing up for beta access!</h1> <h2 style='font-weight: normal; font-size: 19px;'>Stay tuned around Late June! 🫡</h2> <br/> <br/> <h2 style='font-weight: semi-bold; font-size: 19px;'> Join our discord server: <a href='https://discord.gg/nxeUX3Uufn'>https://discord.gg/nxeUX3Uufn</a></h2> <br/><h2 style='font-weight: semi-bold; font-size: 19px;'> Business Queries Only on- <a href='mailto:business@ownboon.com'>business@ownboon.com</a></h2>",
